5dddd28261d0605a75d79764f970cc7754296eb9
Muhammad Zaidi <smazaidi2003@gmail.com>
2021-05-02 18:44:55 -0400

    added misc commands

---
+++ cogs/misc.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Misc(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("misc.py is ready")
+
+    @commands.command()
+    async def ping(self, ctx):
+        embed = discord.Embed(
+            title="Your Latency ⏰", description=f'Your ping is {round(self.client.latency * 1000)}ms!', color=0xFF0000)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def waterloo(self, ctx):
+        await ctx.send("WATER WATER WATER")
+        await ctx.send("LOO LOO LOO")
+
+    @commands.command()
+    async def say(self, ctx, *, message):
+        await ctx.message.delete()
+
+        await ctx.send(message)
+
+    @commands.command()
+    async def avatar(self, ctx, member: discord.Member = None):
+        if not member:
+            member = ctx.author
+        embed = discord.Embed(
+            color=ctx.author.color)
+        embed.set_image(url=member.avatar_url)
+        await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Misc(client))
