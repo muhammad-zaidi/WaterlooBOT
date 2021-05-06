374e0acd3d2ac74860af009730e3ab888034e063
Muhammad Zaidi <smazaidi2003@gmail.com>
2021-05-05 22:24:57 -0400

    update

---
+++ cogs/test.py
@@ -0,0 +1,4 @@
+s = [1, 1, 2]
+m = s
+for i in range(len(s)-1, -1, -1):
+    print(i)
--- cogs/translation.py
+++ cogs/translation.py
@@ -1,3 +1,20 @@
 import discord
 from discord.ext import commands
 from translate import Translator
+
+
+class translation(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("translation.py is ready")
+
+    @commands.command()
+    async def translate(self, ctx):
+        await ctx.send("test")
+
+
+def setup(client):
+    client.add_cog(translation(client))
