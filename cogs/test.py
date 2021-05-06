1cd1b290fdbc2d87423f7d03c23f67610c74a033
Muhammad Zaidi <smazaidi2003@gmail.com>
2021-05-05 22:57:29 -0400

    Finished translation command

--- cogs/test.py
+++
@@ -1,4 +0,0 @@
-s = [1, 1, 2]
-m = s
-for i in range(len(s)-1, -1, -1):
-    print(i)
--- cogs/translation.py
+++ cogs/translation.py
@@ -12,8 +12,19 @@ class translation(commands.Cog):
         print("translation.py is ready")
 
     @commands.command()
-    async def translate(self, ctx):
-        await ctx.send("test")
+    async def translate(self, ctx, *, message):
+        if message == "help":
+            await ctx.send(
+                "Type !translation x y [the word/sentence you want to be translated] where x and y are the ISO 639-1 CODES of the languages you want to be translated.")
+            await ctx.send(
+                "x is the language you want to translate from. y is the language you want to translate to")
+        else:
+            init_lang = message[0:2]
+            to_lang = message[3:5]
+            trans_message = message[6:]
+            translator = Translator(from_lang=init_lang, to_lang=to_lang)
+            translation = translator.translate(trans_message)
+            await ctx.send(translation)
 
 
 def setup(client):
